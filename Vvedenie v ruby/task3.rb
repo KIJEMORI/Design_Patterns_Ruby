09d2791dddc9e1a4f651b0c431b0346f375a91e3
KIJEMORI <kijemori@gmail.com>
2024-09-14 13:31:51 +0300

    Add files via upload

--- Vvedenie v ruby/task3.rb
+++ Vvedenie v ruby/task3.rb
@@ -1,8 +1,10 @@
 #======== Задание 3 ========
 puts "Введите команду Ruby"
 command_for_ruby = STDIN.gets.chomp
+#вызов команды ruby
 eval(command_for_ruby)
 
 puts "Введите команду ОС"
 command_for_console_OS = STDIN.gets.chomp
+#Вызов системной команды
 system(command_for_console_OS)
