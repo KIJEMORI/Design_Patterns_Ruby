c61b011d7afc35d926b4afc3e4cf4e3c06a6b41d
KIJEMORI <kijemori@gmail.com>
2024-09-14 13:47:34 +0300

    Add files via upload

--- Vvedenie v ruby/task6.rb
+++ Vvedenie v ruby/task6.rb
@@ -1,43 +1,52 @@
 #==================================== Задание 6
+# Написать программу, которая принимает как аргумент два
+# значения. Первое значение говорит, какой из методов задачи 1
+# выполнить, второй говорит о том, откуда читать список аргументом
+# должен быть написан адрес файла. Далее необходимо прочитать массив
+# и выполнить метод.
 
+#Метод поиска минимального элемента в массиве
 def search_min_in_array(array)
   min_number = array[0]
-  for i in array do
-    min_number = i if (i < min_number)
+  for item in array do
+    min_number = item if (item < min_number)
   end
   return min_number
 end
 
-def search_items_in_array(array, number)
-  for i in array do
-    return true if i == number
+#Метод поиска максимального элемента в массиве
+def search_max_in_array(array)
+  max_number = array[0]
+  for item in array do
+    max_number = item if (item > max_number)
   end
-  return false
+  return max_number
 end
 
+#Метод поиска первого вхождения положительного элемента в массиве, возвращает индекс, дня номера прибавить 1
 def search_index_first_positiv_number_in_array(array)
-  for i in 0..array.size-1 do
-    if(array[i] > 0) then return i end
+  for index in 0..array.size-1 do
+    if(array[index] > 0) then return index end
   end
   return -1
 end
 
-
+#Проверка существования названия файла для записи массива
 if ARGV[1] != nil then
   string = File.open("#{ARGV[1]}.txt"){ |file| file.read }
+  #Вывод считанного файла
   puts string
+  #Разделение элементов строки на элементы массива
   array_from_file = string.split(%r{ \s*}) 
   
   for i in 0..array_from_file.size-1 do
-    array_from_file[i] = array_from_file[i].to_i
+    array_from_file[i] = Float(array_from_file[i])
   end
   
   if (ARGV[0] == "1") then
     result = search_min_in_array(array_from_file)
   elsif (ARGV[0] == "2") then
-    puts "Введите элемент для поиска"
-    search_item = STDIN.gets
-    result = search_items_in_array(array_from_file, search_item)
+    result = search_max_in_array(array_from_file)
   elsif (ARGV[0] == "3") then
     result = search_index_first_positiv_number_in_array(array_from_file)
   end
