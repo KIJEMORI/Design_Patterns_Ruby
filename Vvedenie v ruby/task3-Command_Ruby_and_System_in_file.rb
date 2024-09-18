e02f6faa48d751039debd9ce37c2667e08a1a9dd
KIJEMORI <kijemori@gmail.com>
2024-09-18 16:16:25 +0300

    Add files via upload

--- Vvedenie v ruby/task3-Command_Ruby_and_System_in_file.rb
+++ Vvedenie v ruby/task3-Command_Ruby_and_System_in_file.rb
@@ -2,9 +2,14 @@
 puts "Введите команду Ruby"
 command_for_ruby = STDIN.gets.chomp
 #вызов команды ruby
-eval(command_for_ruby)
+begin
+  eval(command_for_ruby)
+rescue 
+  puts "Возникла ошибка"
+end
 
 puts "Введите команду ОС"
 command_for_console_OS = STDIN.gets.chomp
 #Вызов системной команды
+
 system(command_for_console_OS)
