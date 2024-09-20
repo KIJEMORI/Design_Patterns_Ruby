56518a8389f65f96db243a819b8ef93aee809283
KIJEMORI <kijemori@gmail.com>
2024-09-20 12:46:49 +0300

    Add files via upload
    добавил проверку существования ARGV

--- Vvedenie v ruby/task2-Hello_user_and_favorite_language.rb
+++ Vvedenie v ruby/task2-Hello_user_and_favorite_language.rb
@@ -1,21 +1,30 @@
 #======== Задание 2 ========
+
+# Проверяем задали ли имя пользователя
+if ARGV.length == 0
+	puts("Пожалуйста, укажите ваше имя")
+	# Завершаем программу
+	exit
+end
+
 #Переменная для имени пользователя, введёного пользователем как аргумент при запуске программы
 name_of_user = ARGV[0]
+
 #Создание строки с форматированием
 str_hello = sprintf( "Hello, %s", name_of_user)
 #Вывод фразы Hello, "Имя пользователя"
 puts str_hello
-
+puts "-------------------"
 
 puts "Какой ваш любимый язык?"
 #Переменная в которую записывается любимый язык пользователя
-favorite_language = STDIN.gets.chomp
+favorite_language = STDIN.gets.chomp.downcase
 #Сравнение введённого любимого языка с потенциальными ответами
-if favorite_language == "Ruby" || favorite_language == "RUBY" || favorite_language == "ruby" || favorite_language == "руби" || favorite_language == "Руби" 
+if favorite_language == "ruby" || favorite_language == "руби" 
     then puts "Подлиза" 
-elsif favorite_language == "Python" || favorite_language == "python"
+elsif favorite_language == "python" || favorite_language == "питон"
     then puts "Хорош"
-elsif favorite_language == "C++" || favorite_language == "С++" || favorite_language == "C#" || favorite_language == "С#" 
+elsif favorite_language == "c++" || favorite_language == "с++" || favorite_language == "c#" || favorite_language == "с#" 
     then puts "Ладно"
 else
     puts "Скоро будет Ruby"
