b574b59efd3ea32a8c7b94a6c273c947b81ed5e8
KIJEMORI <kijemori@gmail.com>
2024-10-04 20:20:10 +0300

    Lab 2 | Task 1.10 | set contacts Student
    Дописал функцию принимающую хэш и меняющую поля студента такие как телефон телеграм почта и гитхаб аккаунт, а так же удалил все сеттеры отвечающие за контакты

--- Ruby_Kernel_classes_and_models/class_Studend.rb
+++ Ruby_Kernel_classes_and_models/class_Studend.rb
@@ -32,37 +32,17 @@ class  Student
   end
 
   def get_full_name
-    @last_name+" "+@first_name+" "+String(@surname)
+    String(@last_name)+" "+String(@first_name)+" "+String(@surname)
   end
   
   #Геттеры и сеттеры в одну строчку
   attr_accessor :last_name, :first_name, :surname
   attr_reader :ID, :phone_number, :telegram_account, :mail, :github_account
 
-  def phone_number=(phone)
-    raise "Некорректный номер телефона"  if Student.is_phone_number(phone) == false
-    @phone_number = phone
-  end
-
-  def telegram_account=(telegram)
-    raise "Некорректный номер телефона"  if Student.is_telegram_account(telegram) == false
-    @telegram_account = telegram
-  end
-
-  def mail=(new_mail)
-    raise "Некорректный номер телефона"  if Student.is_mail(new_mail) == false
-    @mail = new_mail
-  end
-
-  def github_account=(github)
-    raise "Некорректный номер телефона"  if Student.is_github_account(github) == false
-    @github_account = github
-  end
-
   #Получить полную информацию
   def to_s
-    all_info =   String(@ID)+ ") "  + @last_name
-    all_info +=  " "                + @first_name
+    all_info =   String(@ID)+ ") "  + String(@last_name)
+    all_info +=  " "                + String(@first_name)
     all_info +=  " "                + @surname          if @surname           != nil
     all_info +=  "; Phone: "        + @phone_number     if @phone_number      != nil
     all_info +=  "; Telegram: "     + @telegram_account if @telegram_account  != nil
@@ -115,62 +95,21 @@ class  Student
     else
       contacts["Contact"] = "Есть телефон"
     end
-    
+
     return contacts
   end
-end
 
+  def set_contacts(options = {})
+    raise "Некорректный номер телефона"  if Student.is_phone_number(options["Phone"]) == false
+    @phone_number     = options["Phone"]
 
-#====================== MAIN ========================
+    raise "Некорректный телеграм аккаунт"  if Student.is_telegram_account(options["Telegram"]) == false
+    @telegram_account = options["Telegram"]
 
-#Открытие файла с данными и чтение их в массив
-lines_from_file = []
-file = File.open("main.rb","r")
+    raise "Некорректная почта"  if Student.is_mail(options["Mail"]) == false
+    @mail             = options["Mail"]
 
-while !file.eof?
-  lines_from_file  << file.readline
-end
-
-file.close
-
-#Инициализация массива для элементов класса Student
-Students = []
-
-#Разбиение каждой строки из файла на элементы для заполнения элементов класса Student
-for index in 0..lines_from_file.size-1 do
-  #разбиение на каждый параметр с его значением
-  array_of_date = lines_from_file[index].split(";")
-
-  #ФИО обязательное для заполнения
-  name = array_of_date[0]
-  array_without_name = array_of_date-[name]
-  
-  #Массив для заполнения не обязательных данных студентов со структурой ["Тип данных", "данные", ...]
-  stats = []
-  for index in 0..array_without_name.size-1 do
-    array_without_name[index].split(" ", 2).each{|x| stats<<x}
+    raise "Некорректная ссылка на гитхаб аккаунт"  if Student.is_github_account(options["Github"]) == false
+    @github_account   = options["Github"]
   end
-
-  #Создание хэша для заполнения необязательных параметров при создание переменной типа Student
-  hash = Hash[]
-  
-  if stats.size > 0 then
-    index = 0
-    while index < stats.size-1 do
-      hash[stats[index].delete ":"] = stats[index+1]
-      index+=2
-    end
-  end
-  
-  #переменная типа Student
-  std = Student.new(name, hash)
-  #запись студента в массив
-  Students << std
-  
 end
-#Вывод всех данных о каждом студенте
-
-Students.each {|n| puts n}
-
-puts Students[0].validate
-
