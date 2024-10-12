48acc2eb3fa65eb453444b0c8db9300a3565e737
KIJEMORI <kijemori@gmail.com>
2024-10-12 09:05:06 +0300

    Lab 2 | Task 2 | main file
    Переписан файл запускающий программу, теперь в нём нет ничего кроме запуска методов из файла class_Studend.rb

--- Ruby_Kernel_classes_and_models/main.rb
+++ Ruby_Kernel_classes_and_models/main.rb
@@ -1,63 +1,14 @@
 require_relative 'class_Studend'
 
 #====================== MAIN ========================
-class String
-  def to_Student(string_full_info = self)
-    #puts string_full_info
-    #разбиение на каждый параметр с его значением
-    array_of_date = string_full_info.split(";")
 
-    #ФИО обязательное для заполнения
-    name = array_of_date[0]
-    array_without_name = array_of_date-[name]
-
-    #Массив для заполнения не обязательных данных студентов со структурой ["Тип данных", "данные", ...]
-    stats = []
-    for index in 0..array_without_name.size-1 do
-      array_without_name[index].split(" ", 2).each{|x| stats<<x}
-    end
-
-    #Создание хэша для заполнения необязательных параметров при создание переменной типа Student
-    hash = Hash[]
-
-    if stats.size > 0 then
-      index = 0
-      while index < stats.size-1 do
-        hash[stats[index].delete ":"] = stats[index+1]
-        index+=2
-      end
-    end
-    
-    return Student.new(name, hash)
-  end
-end
-
-#Открытие файла с данными и чтение их в массив
-lines_from_file = []
-file = File.open("list_students.rb","r")
-
-while !file.eof?
-  lines_from_file  << file.readline
-end
-
-file.close
-
-#Инициализация массива для элементов класса Student
-Students = []
-
-#Разбиение каждой строки из файла на элементы для заполнения элементов класса Student
-for index in 0..lines_from_file.size-1 do
-  
-  #переменная типа Student
-  begin
-    std = lines_from_file[index].to_Student
-  rescue => e
-    puts e
-  end
-  #запись студента в массив
-  Students << std
-  
-end
+Students = read_from_txt("list_students.rb")
 #Вывод всех данных о каждом студенте
 
 Students.each {|n| puts n}
+
+std = Student_short.new(student: Students[0])
+puts std
+std = Student_short.new(info: "Аврелий Майски Доводчик; phone: 88005553535")
+puts std
+
