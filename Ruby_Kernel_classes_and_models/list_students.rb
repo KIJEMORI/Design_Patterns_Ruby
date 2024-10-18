da05efb9994aa108ca8145f5e9e3f633ca9499e3
KIJEMORI <kijemori@gmail.com>
2024-10-18 17:20:24 +0300

    Lab 2 | Task 2 | Rename setters and getters
    переименованы геттеры и сеттеры, изменена структура хэша в сеттере имени фамилии и отчества класса Student

--- Ruby_Kernel_classes_and_models/list_students.rb
+++ Ruby_Kernel_classes_and_models/list_students.rb
@@ -1,3 +1,3 @@
-Иванов Иван Иваниович; Phone: 8-090-897-98-87; Telegram: @xxxxyyyy; Mail: lol@mail.ru; Github: https://github.com/KIJEMORI
+Иванов Иван Иваниович; phone: 8-090-897-98-87; telegram: @xxxxyyyy; mail: lol@mail.ru; github: https://github.com/KIJEMORI
 Петров Пётр Петрович
-Гнидкинс Валентин; Phone: 8-800-555-35-35
+Гнидкинс Валентин; phone: 8-800-555-35-35
