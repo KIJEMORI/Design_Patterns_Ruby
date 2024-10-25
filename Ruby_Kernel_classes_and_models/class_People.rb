dd4bcfb416555536552b8342ab4f71d7004519d2
KIJEMORI <kijemori@gmail.com>
2024-10-25 16:58:45 +0300

    Lab 2 | Task 2 | add github to people
    Добавил поле github к классу People, введён метод класса is_github? для проверки строки на соблюдения правил введения github аккаунта, изменён метод to_s для вывода github в конце строки

--- Ruby_Kernel_classes_and_models/class_People.rb
+++ Ruby_Kernel_classes_and_models/class_People.rb
@@ -1,18 +1,36 @@
+
 class People
-  def initialize(id)
-    @id = id
+  def initialize(options = {id: nil, "github": nil})
+    @id = options[:id]
+    raise "Некорректная ссылка на гитхаб аккаунт"  if People.is_github?(options["github"]) == false
+    @github = options["github"]
   end
-  attr_reader :id
+
+  def People.is_github? (maybe_github)
+    regex = /  ^https: \/ \/ github\.com \/ [a-zA-Z\d\_\-]{5,32} $  /x
+    maybe_github.match?(regex) if maybe_github != nil
+  end
+
+  attr_reader :id, :github
 #Получение полной информации исходя из переменной объекта класса
   def to_s
-    instance_variables.map do |type|
+    information = instance_variables.map do |type|
       value = instance_variable_get(type)
       type = type.to_s.delete_prefix('@')
-      if type == "last_name" || type == "first_name" || (type == "surname" && value != nil) || type == "name" then
+      if type == "last_name" || type == "first_name" || (type == "surname" && value != nil) || type == "last_name_and_initials" then
         "#{value}"
-      else 
+      elsif type != "github"
         "; #{type.capitalize}: #{value}" if value != nil
       end
     end.join(" ").gsub("  ", " ").gsub(" ;", ";").sub(" ", "")
+
+    
+    information += "; #{"github".capitalize}: #{@github}" if @github != nil
+    
+    return information
+  end
+
+  def last_name_and_initials
+
   end
 end
