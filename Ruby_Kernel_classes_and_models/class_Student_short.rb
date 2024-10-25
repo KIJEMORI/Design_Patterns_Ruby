5c8b7a44a5edeaf77ca075ae7fe6a3695bf5c5c2
KIJEMORI <kijemori@gmail.com>
2024-10-25 17:01:13 +0300

    Lab 2 | Task 2 | refactoring super()
    теперь в вызов супер класса однозначно подаётся hash и данные о github

--- Ruby_Kernel_classes_and_models/class_Student_short.rb
+++ Ruby_Kernel_classes_and_models/class_Student_short.rb
@@ -3,19 +3,24 @@ require_relative 'class_People'
 class Student_short < People
 
   def initialize(options ={student: nil, info: nil, id: nil})
-    super(options[:id])
+    
 
     if(!options[:student] && options[:info]!=nil) then
       options[:student] = options[:info].to_Student
     end
     
     raise "Нет данных для заполнения объекта Student_short" if (!options[:student])
-      
-    @name = options[:student].last_name_and_initials
+    option = Hash[]
+    option[:id] = options[:id]
+    option["github"] = options[:student].github
+
+    super(option)
+
+    @last_name_and_initials = options[:student].last_name_and_initials
     @contact = options[:student].one_of_contacts
-    @github = options[:student].github
+    
 
   end
 
-  attr_reader :name, :contact, :github
+  attr_reader :last_name_and_initials, :contact
 end
