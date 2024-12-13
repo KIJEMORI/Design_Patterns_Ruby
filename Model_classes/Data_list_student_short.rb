3c4a1c9329667f7245c1e43909443eb2d7869b39
KIJEMORI <kijemori@gmail.com>
2024-12-13 22:50:25 +0300

    Lab 4 | Task 5 | Create class Data_list_student_short
    Создал класс наследник который использует методы родителя в переопределённом методе

---
+++ Model_classes/Data_list_student_short.rb
@@ -0,0 +1,17 @@
+require_relative 'Student_short'
+require_relative 'Data_list'
+class Data_list_student_short < Data_list
+
+  def initialize()
+    super()
+  end
+
+  def get_names
+    return ["№", "last_name_and_initials", "github", "one_of_contacts"]
+  end
+
+  def attributes(student_short)
+    return [student_short.last_name_and_initials, student_short.github, student_short.one_of_contacts]
+  end
+
+end
