7e8cabe9c518a40e33078b33502be8410e2bb64f
KIJEMORI <kijemori@gmail.com>
2024-12-13 21:10:38 +0300

    Lab 4 | Task 4 | create methods
    создал методы. Метод select записывает в массив выделенные элементы по индексу в списке, или же удаляет элементы из выделенных. Метод get_selected возвращает поле id выделенных элементов. Метод get_names не реализован. Метод get_data возвращает объект класса Data_table, но не реализован

--- Model_classes/Data_list.rb
+++ Model_classes/Data_list.rb
@@ -1,7 +1,48 @@
+require_relative 'Data_table'
+
 class Data_list
 
   def initialize(data):
     @array = data
+
+    @selected = []
+  end
+
+  def select(number)
+    raise "Неверный индекс" if (number > array.size()-1 || number < 0)
+    if(!@selected.include?(number)) then
+      @selected << @array[number]
+    else
+      index = selected_id.find_index(number)
+      @selected.delete_at(index)
+    end
+  end
+
+  def get_selected()
+    seleceted_array_id = []
+    for index in 0..@array.size
+      seleceted_array_id << @array[index].id if(@selected.include(index))
+    end
+    return seleceted_array
+  end
+
+  def get_names()
+    raise NotImplementedError, "Не реализован"
+  end
+
+  def get_data()
+
+    table = []
+    for index in 0..@array.size()
+      table << ([index] + attributes())
+    end
+
+    return Data_table.new(table)
+    
+  end
+
+  def attributes()
+    raise NotImplementedError, "Не реализован"
   end
 
 end
