ebdd56b50fc6421d0b858d41f2ab4d7d02a3ae53
KIJEMORI <kijemori@gmail.com>
2024-12-13 20:59:15 +0300

    Lab 4 | Task 2 | create methods
    Создал методы позволяющие получить количество строк и столбцов в таблице, а также метод позволяющий получить элемент в зависимости от введённой строки и столбца

--- Model_classes/Data_list.rb
+++ Model_classes/Data_list.rb
@@ -4,4 +4,26 @@ class Data_list
       @matrix = data
   end
 
+  def count_row()
+        return @matrix.size()
+    end
+
+    def count_column()
+        max = 0
+        for index in 0..@matrix.size:
+            if(@matrix[index].size()> max):
+                max = @matrix[index].size()
+            end
+        end
+        return max
+    end
+
+    def get(row, col)
+        raise "Значение строки за пределами двумерного массива" if (row > count_row() - 1 || row < 0)
+        raise "Значение столбца за пределами двумерного массива" if (col > @matrix[row].size()-1 || col < 0)
+
+        item = @matrix[row][col].copy(0)
+        return item
+    end
+  
 end
